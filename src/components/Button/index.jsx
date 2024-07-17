27ca599bb73d22ba5340511f9c448acbbdc98ca9
Thomas Blanc <thoblanc.tb@gmail.com>
2024-07-17 14:06:55 +0200

    correctino taille bouton

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,12 +5,12 @@ export const Button = ({ Name, Url }) => {
   return (
     <div className=" max-width-full text-center my-7">
       <Link
-        className="group relative inline-block focus:outline-none focus:ring xl:min-w-[266px]"
+        className="group relative inline-block focus:outline-none focus:ring md:min-w-[266px]"
         to={Url}
       >
-        <span className="xl:min-w-[266px] absolute inset-0 translate-x-1.5 translate-y-1.5 bg-blue-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
+        <span className="md:min-w-[266px] absolute inset-0 translate-x-1.5 translate-y-1.5 bg-blue-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
 
-        <span className="xl:min-w-[266px] relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75">
+        <span className="md:min-w-[266px] relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75">
           {Name}
         </span>
       </Link>
